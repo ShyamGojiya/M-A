3ca1764bf2b892c5335facf51824c766d6cae82e
jani <jani@gmail.com>
2024-10-19 16:18:12 +0530

    Profile Page

--- frontend/src/components/home/Profile.js
+++ frontend/src/components/home/Profile.js
@@ -35,33 +35,66 @@ const Profile = () => {
         <Loader />
       ) : (
         <Fragment>
-          <div className="profileContainer">
-            <div>
-              <h1>My Profile</h1>
-              {/* <img src={user.avatar.url} alt={user.name} /> */}
-              <Link to="/me/update">Edit Profile</Link>
-            </div>
-            <div>
-              <div>
-                <h4>Full Name</h4>
-                {user && <p>{user.name}</p>}
-              </div>
-              <div>
-                <h4>Email</h4>
-                <p>{user.email}</p>
-              </div>
-              <div>
-                <h4>Joined On</h4>
-                <p>{String(user.createdAt).substr(0, 10)}</p>
+          <div className="profileContainer h-screen bg-slate-200 flex gap-3 p-6 flex-col md:flex-row">
+            <aside className="w-full md:w-1/5 bg-white shadow-lg rounded-lg p-4 sticky h-[80%] flex-shrink-0">
+              <h2 className="text-green-600 font-bold text-2xl mb-6">Navigation</h2>
+
+              <div className="flex justify-center mb-4">
+                <img src={user.avatar.url} alt={user.name} className="bg-slate-400 w-32 h-32 rounded-full" />
               </div>
 
-              <div>
-                <Link to="/orders">My Orders</Link>
-                <Link to="/password/update">Change Password</Link>
-                <button onClick={handleLogout}>Logout</button>
+              <ul className="space-y-4">
+                <li>
+                  <Link to="/me/update" className="block font-semibold text-lg text-green-600 border-2 border-green-600 rounded-lg p-2 text-center hover:bg-green-100 transition duration-300">Edit Profile</Link>
+                </li>
+                <li>
+                  <Link to="/orders" className="block font-semibold text-lg text-green-600 border-2 border-green-600 rounded-lg p-2 text-center hover:bg-green-100 transition duration-300">My Orders</Link>
+                </li>
+                <li>
+                  <Link to="/password/update" className="block font-semibold text-lg text-green-600 border-2 border-green-600 rounded-lg p-2 text-center hover:bg-green-100 transition duration-300">Change Password</Link>
+                </li>
+                <li>
+                  <button onClick={handleLogout} className="w-full font-semibold text-lg text-red-600 hover:text-red-500 transition duration-300 border-2 border-red-600 rounded-lg p-2 text-center hover:bg-red-100">Log Out</button>
+                </li>
+              </ul>
+            </aside>
+
+            <main className="flex-1 bg-white shadow-lg rounded-lg p-6 h-[80%]">
+              <div className="flex flex-col items-start transition-transform transform h-full">
+                <h1 className="text-green-600 font-bold text-3xl mb-4">My Profile</h1>
+
+                <div className="mb-4">
+                  <h4 className="text-green-600 font-semibold text-lg">Full Name:</h4>
+                  {user && <p className="text-slate-500 text-lg font-bold">{user.name}</p>}
+                </div>
+                <div className="mb-4">
+                  <h4 className="text-green-600 font-semibold text-lg">Email:</h4>
+                  <p className="text-slate-500 text-lg font-bold">{user.email}</p>
+                </div>
+                <div className="mb-4">
+                  <h4 className="text-green-600 font-semibold text-lg">Mobile No. :</h4>
+                  <p className="text-slate-500 text-lg font-bold">+91 {user.mobile}</p>
+                </div>
+                <div className="mb-4">
+                  <h4 className="text-green-600 font-semibold text-lg">Joined On:</h4>
+                  <p className="text-slate-500 text-lg font-bold">{new Date(user.createdAt).toLocaleDateString()}</p>
+                </div>
               </div>
-            </div>
+            </main>
           </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
         </Fragment>
       )}
     </Fragment>
